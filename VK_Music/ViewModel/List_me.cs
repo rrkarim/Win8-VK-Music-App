15258929d912f38032653d1d7f7204340a7279b5
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-11 23:16:07 +0400

    refactoring

--- VK_Music/ViewModel/List_me.cs
+++ VK_Music/ViewModel/List_me.cs
@@ -18,7 +18,7 @@ namespace VK_Music.ViewModel
         public Me_ListViewModel()
         {
             ListItems = new ObservableCollection<ListItem_Me>();
-            ListItems.Add(new ListItem_Me { id = 0, Name = "Мои АудиоЗаписи" });
+            ListItems.Add(new ListItem_Me { id = 0, Name = "Мои Аудиозаписи" });
             ListItems.Add(new ListItem_Me { id = 1, Name = "Рекомендации" });
             ListItems.Add(new ListItem_Me { id = 2, Name = "Популярное" });
         }
